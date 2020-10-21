9ecff33ecf4c69db01414882a66be87eed2c0415
Kesavan <kesavan.keshav@gmail.com>
2020-10-21 19:53:02 +0530

    installed express framework

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
 const http = require('http')
-const { getProducts,getProductById,createProduct,updateProduct,deleteProduct } = require('./controllers/productcontroller')
+const { getProducts,getProductById,
+      createProduct,updateProduct,
+      deleteProduct } = require('./controllers/productcontroller')
 
-const server = http.createServer((req,res) =>{  
-    if(req.url === '/api/products' && req.method === 'GET')
-    {
-        getProducts(req,res);
-    }
-    else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET')
-    {
-        const Id = req.url.split('/')[3]
-        getProductById(req,res,Id)
-    }
-    else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT')
-    {
-        const Id = req.url.split('/')[3]
-        updateProduct(req,res,Id);
-    }
-    else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE')
-    {
-        const Id = req.url.split('/')[3]
-        deleteProduct(req,res,Id);
-    }
-    else if(req.url === '/api/products' && req.method === 'POST')
-    {
-        createProduct(req,res);
-    }
-    else{
-        res.writeHead(404,{'Content-Type':'application/json'})
-        res.end(JSON.stringify({ message:'Route not found' }));
-    }
-});
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
+
+app.route('/api/products')
+      .get(getProducts)
+      .post(createProduct)
+      
 
 const PORT = process.env.PORT || 5000
 
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT,() => {
+  console.log(`Server is listening ${PORT}`)
+})
+
+// const server = http.createServer((req,res) =>{  
+//     if(req.url === '/api/products' && req.method === 'GET')
+//     {
+//         getProducts(req,res);
+//     }
+//     else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET')
+//     {
+//         const Id = req.url.split('/')[3]
+//         getProductById(req,res,Id)
+//     }
+//     else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT')
+//     {
+//         const Id = req.url.split('/')[3]
+//         updateProduct(req,res,Id);
+//     }
+//     else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE')
+//     {
+//         const Id = req.url.split('/')[3]
+//         deleteProduct(req,res,Id);
+//     }
+//     else if(req.url === '/api/products' && req.method === 'POST')
+//     {
+//         createProduct(req,res);
+//     }
+//     else{
+//         res.writeHead(404,{'Content-Type':'application/json'})
+//         res.end(JSON.stringify({ message:'Route not found' }));
+//     }
+// });
+
+
+
+// server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
