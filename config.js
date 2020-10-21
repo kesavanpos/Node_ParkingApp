503ed0eb01a0e670c1e458daf6966f293293c2f9
Kesavan <kesavan.keshav@gmail.com>
2020-10-21 19:20:06 +0530

    adding env variables

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const {Pool} = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
 
-const DATABASE_URL  = process.env.DATABASE_URLS
+const DATABASE_URL  = process.env.DATABASE_URL
 
 const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
