e98731a35b03fa45bd49f5882a2365f0684a98d2
Kesavan <kesavan.keshav@gmail.com>
2020-10-21 18:52:17 +0530

    Added api changes

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
 const http = require('http')
 const { getProducts,getProductById,createProduct,updateProduct,deleteProduct } = require('./controllers/productcontroller')
 
-const {pool} = require('./config')
-
-
-const server = http.createServer((req,res) =>{
-
-  pool.query('SELECT * FROM accounts', (error, results) => {
-    if (error) {
-      throw error
+const server = http.createServer((req,res) =>{  
+    if(req.url === '/api/products' && req.method === 'GET')
+    {
+        getProducts(req,res);
     }
-
-    res.writeHead(200,{'Content-Type':'application/json'})
-    res.end(JSON.stringify(results.rows));
-  })
-
-    // if(req.url === '/api/products' && req.method === 'GET')
-    // {
-    //     getProducts(req,res);
-    // }
-    // else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET')
-    // {
-    //     const Id = req.url.split('/')[3]
-    //     getProductById(req,res,Id)
-    // }
-    // else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT')
-    // {
-    //     const Id = req.url.split('/')[3]
-    //     updateProduct(req,res,Id);
-    // }
-    // else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE')
-    // {
-    //     const Id = req.url.split('/')[3]
-    //     deleteProduct(req,res,Id);
-    // }
-    // else if(req.url === '/api/products' && req.method === 'POST')
-    // {
-    //     createProduct(req,res);
-    // }
-    // else{
-    //     res.writeHead(404,{'Content-Type':'application/json'})
-    //     res.end(JSON.stringify({ message:'Route not found' }));
-    // }
+    else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET')
+    {
+        const Id = req.url.split('/')[3]
+        getProductById(req,res,Id)
+    }
+    else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT')
+    {
+        const Id = req.url.split('/')[3]
+        updateProduct(req,res,Id);
+    }
+    else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE')
+    {
+        const Id = req.url.split('/')[3]
+        deleteProduct(req,res,Id);
+    }
+    else if(req.url === '/api/products' && req.method === 'POST')
+    {
+        createProduct(req,res);
+    }
+    else{
+        res.writeHead(404,{'Content-Type':'application/json'})
+        res.end(JSON.stringify({ message:'Route not found' }));
+    }
 });
 
 const PORT = process.env.PORT || 5000
